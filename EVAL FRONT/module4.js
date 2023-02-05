42aaa4b40672434c98661a386df12cf319046205
killard95 <m.benattou@live.fr>
2023-02-05 16:56:16 +0100

    derniére modif

--- EVAL FRONT/module4.js
+++ EVAL FRONT/module4.js
@@ -1,35 +1,40 @@
-        // Module4
+// Module4
 
-        let ul = document.getElementById("ulModule4");
-        let arrLi = ul.getElementsByTagName("li");
-        let up = document.getElementById("up")
-        let down = document.getElementById("down")
-        let li1 = arrLi[0]
-        let li2 = arrLi[1]
-        let li3 = arrLi[2]
-        let li4 = arrLi[3]
-   
-        
-        up.addEventListener("click", ()=>{ 
-            if (li1 == arrLi[1] ) {
-                li2.insertAdjacentElement('beforebegin', li1);
-            }else if (li1 == arrLi[2]){
-                li3.insertAdjacentElement('beforebegin', li1);
-            }else if (li1 == arrLi[3]){
-                li4.insertAdjacentElement('beforebegin', li1);
-            } else if (li1 == arrLi[0]){
-                li4.insertAdjacentElement('afterend', li1);
-            }
-        })
+    // Variables des ééments du module
+    let ul = document.getElementById("ulModule4");
+    let arrLi = ul.getElementsByTagName("li");
+    let up = document.getElementById("up")
+    let down = document.getElementById("down")
+    let li1 = arrLi[0]
+    let li2 = arrLi[1]
+    let li3 = arrLi[2]
+    let li4 = arrLi[3]
 
-        down.addEventListener("click", ()=>{
-            if (li1 == arrLi[0] ) {
-                li2.insertAdjacentElement('afterend', li1);
-            }else if (li1 == arrLi[1]){
-                li3.insertAdjacentElement('afterend', li1);
-            }else if (li1 == arrLi[2]){
-                li4.insertAdjacentElement('afterend', li1);
-            } else if (li1 == arrLi[3]){
-                li2.insertAdjacentElement('beforebegin', li1);
-            }
-        })
+    // On ajoute un événements au clic sur le bouton up
+    up.addEventListener("click", ()=>{
+        // On ajoute des conditions pour que la ligne 1 grimpe d'une position
+        if (li1 == arrLi[1] ) {
+            li2.insertAdjacentElement('beforebegin', li1);
+        }else if (li1 == arrLi[2]){
+            li3.insertAdjacentElement('beforebegin', li1);
+        }else if (li1 == arrLi[3]){
+            li4.insertAdjacentElement('beforebegin', li1);
+        } else if (li1 == arrLi[0]){
+            li4.insertAdjacentElement('afterend', li1);
+        }
+    })
+
+    // On ajoute un événements au clic sur le bouton up
+
+    down.addEventListener("click", ()=>{
+        // On ajoute des conditions pour que la ligne 1 descende d'une position
+        if (li1 == arrLi[0] ) {
+            li2.insertAdjacentElement('afterend', li1);
+        }else if (li1 == arrLi[1]){
+            li3.insertAdjacentElement('afterend', li1);
+        }else if (li1 == arrLi[2]){
+            li4.insertAdjacentElement('afterend', li1);
+        } else if (li1 == arrLi[3]){
+            li2.insertAdjacentElement('beforebegin', li1);
+        }
+    })
--- EVAL FRONT/module5.js
+++ EVAL FRONT/module5.js
@@ -1,41 +1,52 @@
-        // Module 5
-        let myDiv = document.getElementById("cibleModule5");
-        let nbrCheck = "";
-        let check = document.getElementById("checkboxModule5");
-        let menu = document.createElement("select")
-        let newDiv = document.createElement("div");
-        
-        
-        let radio = document.getElementById("radioModule5");
-        check.addEventListener("change", ()=>{
-            for (let i=0;i<check.length;i++){
-                if (check[i].checked === true && ! nbrCheck.includes(check[i].value)){
-                    nbrCheck += `${check[i].value} `
-                } else if (check[i].checked === false){
-                    nbrCheck = nbrCheck.replace(check[i].value,"")
-                }
-                radio.addEventListener("change", ()=>{
-                    if (radio[0].checked === true){
-                        if (myDiv.hasChildNodes()){
-                            myDiv.removeChild(myDiv.children[0])
-                        }
-                        myDiv.appendChild(newDiv);
-                        newDiv.innerHTML = nbrCheck
-                    } else if (radio[1].checked === true){
-                        if (myDiv.hasChildNodes()){
-                            myDiv.removeChild(myDiv.children[0])
-                        }
-                        console.log(nbrCheck)
-                        myDiv.appendChild(menu);
-                            let option = document.createElement("option");
-                            menu.appendChild(option);
-                            option.innerHTML = nbrCheck;
-                            
-                            
-                        }   
-                    })
-                }
-        });
+// Module 5
+
+    // Variables pour récupérer les éléments du module
+    let myDiv = document.getElementById("cibleModule5");
+    let check = document.getElementById("checkboxModule5");
+    let menu = document.createElement("select")
+    let newDiv = document.createElement("div");
+    let radio = document.getElementById("radioModule5");
+    // Variable vide pour stocker les checkbox checked
+    let nbrCheck = "";
+    
+    // Ajout d'un premier évenement au changement des checkbox
+    check.addEventListener("change", ()=>{
+        // on boucle pour analyser chacun des checkbox
+        for (let i=0;i<check.length;i++){
+            // Conditions si un checkbox est checked et s'il n'est pas déja enregistré dans la variable nbrCheck
+            if (check[i].checked === true && ! nbrCheck.includes(check[i].value)){
+                // Si la condition est respecté on ajoute la valeur de la checkbox checked dans la variable nbrCheck
+                nbrCheck += `${check[i].value} `
+                // Sinon on la supprime de nbrCCheck
+            } else if (check[i].checked === false){
+                nbrCheck = nbrCheck.replace(check[i].value,"")
+            }
+            // Ajout d'un deuxiéme événement au changement du "radio"
+            radio.addEventListener("change", ()=>{
+                // Si le radio choisi et "div"
+                if (radio[0].checked === true){
+                    // Si il y a un childNodes on le supprime
+                    if (myDiv.hasChildNodes()){
+                        myDiv.removeChild(myDiv.children[0])
+                    }
+                    // On génére une "div" qu'on attache au parent et on y insére le résultat de la variable nbrCheckbox
+                    myDiv.appendChild(newDiv);
+                    newDiv.innerHTML = nbrCheck
+                // Si le radio choisi et "opt"
+                } else if (radio[1].checked === true){
+                    // Si il y a un childNodes on le supprime
+                    if (myDiv.hasChildNodes()){
+                        myDiv.removeChild(myDiv.children[0])
+                    }
+                    // On génére une "option" qu'on attache au parent et on y insére le résultat de la variable nbrCheckbox
+                    myDiv.appendChild(menu);
+                    let option = document.createElement("option");
+                    menu.appendChild(option);
+                    option.innerHTML = nbrCheck;
+                    }   
+                })
+            }
+    });
         
 
 
